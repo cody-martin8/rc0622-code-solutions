59de01dc93c9e07aa32730c62ff9b98782db1f0d
Cody Martin <12martincody@gmail.com>
2022-07-06 05:36:57 +0000

    Completed javascript-data-model exercise.

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,50 @@
+var orderHistory = [
+  {
+    dateOrderPlaced: 'August 4, 2020',
+    orderTotal: 34.00,
+    shipTo: 'JS Masher',
+    orderNumber: '114-3941689-8772232',
+    numberOfItems: 1,
+    itemTitle: 'JavaScript for impatient programmers',
+    author: 'Rauschmayer, Dr. Axel',
+    deliveredDate: 'Aug 8, 2020',
+    returnWindow: 'Sep 7, 2020',
+    itemPrice: 31.55
+  },
+  {
+    dateOrderPlaced: 'July 19, 2020',
+    orderTotal: 44.53,
+    shipTo: 'JS Masher',
+    orderNumber: '113-9984268-1280257',
+    numberOfItems: 1,
+    itemTitle: 'The Timeless Way of Building',
+    author: 'Alexander, Christopher',
+    deliveredDate: 'Jul 20, 2020',
+    returnWindow: 'Aug 19, 2020',
+    itemPrice: 41.33
+  },
+  {
+    dateOrderPlaced: 'July 4, 2020',
+    orderTotal: 17.22,
+    shipTo: 'JS Masher',
+    orderNumber: '114-2875557-9059409',
+    numberOfItems: 1,
+    itemTitle: 'Gamecube Controller Adapter, Super Smash Bros Switch Gamecube Adapter for Wii U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+    author: null,
+    deliveredDate: 'Jul 7, 2020',
+    returnWindow: 'Aug 19, 2020',
+    itemPrice: 15.98
+  },
+  {
+    dateOrderPlaced: 'July 3, 2020',
+    orderTotal: 138.93,
+    shipTo: 'JS Masher',
+    orderNumber: '113-2883177-2648248',
+    numberOfItems: 2,
+    itemTitle: ['GameCube Controller - Super Smash BroadcastChannel. Edition (Nintendo Switch)', 'The Art of Sql'],
+    author: [null, 'Faroult, Stephane'],
+    deliveredDate: 'Jul 5, 2020',
+    returnWindow: ['Aug 4, 2020', 'Aug 4, 2020'],
+    itemPrice: [94.95, 33.99]
+  }
+];
