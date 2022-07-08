71c01236f06f3cdf4b85a06b6a8a5f0c532a09c0
Cody Martin <12martincody@gmail.com>
2022-07-08 23:42:23 +0000

    Completed object-manipulation-1 exercise.

---
+++ object-manipulation-1/src/get-value.js
@@ -0,0 +1,13 @@
+/* exported getValue */
+// receive object and key as arguments
+// select value by looking at the key property of the object
+// return the selected value
+
+function getValue(object, key) {
+  return object[key];
+}
+
+// receive object and key as arguments
+// select value by looking at the key property of the object
+//   ignore error stating dot notation is better
+// return the selected value
