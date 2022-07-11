5d6526a03143aed5806b7d298040531661bebf0e
Cody Martin <12martincody@gmail.com>
2022-07-11 18:31:58 +0000

    Completed dom-querying exercise.

---
+++ dom-querying/main.js
@@ -0,0 +1,16 @@
+console.log('hello, world');
+
+var h1 = document.querySelector('h1');
+console.log(h1);
+console.dir(h1);
+
+var idExplanation = document.querySelector('#explanation');
+console.log(idExplanation);
+console.dir(idExplanation);
+
+var classHint = document.querySelector('.hint');
+console.log(classHint);
+console.dir(classHint);
+
+console.log(document.querySelectorAll('p'));
+console.log(document.querySelectorAll('.example-link'));
