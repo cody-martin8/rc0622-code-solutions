99d87e156af7a93e92b94bbbfdcedbe57e3fdfcc
Cody Martin <12martincody@gmail.com>
2022-09-02 17:59:32 +0000

    Corrected issue of having multiple states by calculating one on-render.

--- react-hot-button-component/src/hot-button.jsx
+++ react-hot-button-component/src/hot-button.jsx
@@ -1,35 +1,36 @@
 import React from 'react';
 
-export class HotButton extends React.Component {
+export default class HotButton extends React.Component {
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
-    this.state = { color: 'btn-indigo', clicks: 0 };
+    this.state = { clicks: 0 };
   }
 
   handleClick() {
-    let btnColor = 'btn-indigo';
-    if (this.state.clicks > 1) {
-      btnColor = 'btn-purple';
-    }
-    if (this.state.clicks > 4) {
-      btnColor = 'btn-red';
-    }
-    if (this.state.clicks > 7) {
-      btnColor = 'btn-orange';
-    }
-    if (this.state.clicks > 10) {
-      btnColor = 'btn-yellow';
-    }
-    if (this.state.clicks > 13) {
-      btnColor = 'btn-white';
-    }
-    this.setState({ color: btnColor, clicks: this.state.clicks + 1 });
+    this.setState({ clicks: this.state.clicks + 1 });
   }
 
   render() {
+    let btnColor = 'btn-indigo';
+    if (this.state.clicks > 2) {
+      btnColor = 'btn-purple';
+    }
+    if (this.state.clicks > 5) {
+      btnColor = 'btn-red';
+    }
+    if (this.state.clicks > 8) {
+      btnColor = 'btn-orange';
+    }
+    if (this.state.clicks > 11) {
+      btnColor = 'btn-yellow';
+    }
+    if (this.state.clicks > 14) {
+      btnColor = 'btn-white';
+    }
+
     return (
-      <button type="button" className={`btn ${this.state.color}`} onClick={this.handleClick}>Hot Button</button>
+      <button type="button" className={`btn ${btnColor}`} onClick={this.handleClick}>Hot Button</button>
     );
   }
 }
